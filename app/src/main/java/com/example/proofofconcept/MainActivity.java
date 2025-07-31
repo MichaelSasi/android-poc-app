174130cf9197a6d58a196a3391b344a2eba4c4ef
MichaelSasi <77620576+MichaelSasi@users.noreply.github.com>
2025-07-31 15:00:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/proofofconcept/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.proofofconcept;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView homeText = findViewById(R.id.home_text);
+        homeText.setText("Michael is a noob");
+
+        Button homeButton = findViewById(R.id.home_button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("SASI","You have clicked the button");
+                counter++;
+                homeText.setText("Clicked: " + counter);
+
+                if(counter==10){
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://michaelsasi.me"));
+                    startActivity(browserIntent);
+                }
+            }
+        });
+    }
+}
